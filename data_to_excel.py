04bd91afdd6e40a55d61bc601df5bc1436ebed8a
tristanh314 <tristanh314@gmail.com>
2023-10-02 13:27:17 -0700

    Filtering useful data.

--- data_to_excel.py
+++ data_to_excel.py
@@ -1,6 +1,18 @@
 import numpy as np
 import pandas as pd
 
-data_array = pd.read_csv("utah_florida_raw.csv")
+# Read the raw data
+game_data = pd.read_csv('utah_florida_raw.csv')
 
-print(data_array.head())
+# Remove information that will not be used.
+game_data = game_data.drop(columns=['Id','Offense Conference','Defense Conference','Home','Away','Game Id','Drive Id','Offense Timeouts','Defense Timeouts','Yard Line','Ppa','Wallclock'])
+
+i=1
+while i <= game_data.shape[0]:
+    if "Kickoff" in game_data.loc[i,'Play Type']:
+        game_data=game_data.drop(index=i)
+    i+=1
+    
+
+# home_off = game_data.loc[game_data['Offense']=='Utah']
+# home_def = game_data.loc[game_data['Defense']=='Utah']
