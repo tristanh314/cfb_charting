ed44d08885fa98bf3559de1ba7604715d27ac194
tristanh314 <tristanh314@gmail.com>
2023-10-02 11:26:44 -0700

    Pandas can handle commas in play description.

--- data_to_excel.py
+++ data_to_excel.py
@@ -1,6 +1,6 @@
 import numpy as np
+import pandas as pd
 
-data_array = np.loadtxt(fname="utah_florida_raw.csv",
-                          delimiter=",",
-                          dtype=str,)
+data_array = pd.read_csv("utah_florida_raw.csv")
+
 print(data_array.head())
