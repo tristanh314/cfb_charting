9a15ff7d4f8445db6171390e47dd6bf6b0b6e83d
tristanh314 <tristanh314@gmail.com>
2023-10-08 22:12:29 -0700

    Some rows not dropping

--- data_to_excel.py
+++ data_to_excel.py
@@ -1,5 +1,3 @@
-import odswriter
-import data_to_excel
 import numpy as np
 import pandas as pd
 
@@ -9,23 +7,38 @@ game_data = pd.read_csv('utah_florida_raw.csv')
 # Remove information that will not be used.
 game_data = game_data.drop(columns=['Id','Offense Conference','Defense Conference','Home','Away','Game Id','Drive Id','Offense Timeouts','Defense Timeouts','Yard Line','Ppa','Wallclock'])
 
-i=1
+i=0
 while i <= game_data.shape[0]:
-    if 'Kickoff' in game_data.loc[i,'Play Type']:
+    if 'Kickoff' in game_data.loc[i]['Play Type']:
         game_data=game_data.drop(index=i)
-    elif 'Punt' in game_data.loc[i, 'Play Type']:
+    elif 'Punt' in game_data.loc[i]['Play Type']:
         game_data=game_data.drop(index=i)
-    elif 'Field Goal' in game_data.loc[i, 'Play Type']:
+    elif 'Field Goal' in game_data.loc[i]['Play Type']:
+        game_data=game_data.drop(index=i)
+    elif 'Timeout' in game_data.loc[i]['Play Type']:
+        game_data=game_data.drop(index=i)
+    elif 'End of' in game_data.loc[i]['Play Type']:
         game_data=game_data.drop(index=i)
     i+=1
 
+# Create play type data
+def abbrev(string):
+    if 'Pass' in string:
+        return 'p'
+    elif 'Rush' in string:
+        return 'r'
+    else:
+        return '?'
+
+abreviations = game_data['Play Type'].apply(abbrev)
+game_data['Play Abrev'] = abreviations
 # Sort play data by team on offense.
-primary_off = game_data.loc[game_data['Offense']=='Utah']
-secondary_off = game_data.loc[game_data['Offense']=='Florida']
+primary_off = game_data.loc[game_data['Offense']=='Utah'].reset_index().drop(columns='index')
+secondary_off = game_data.loc[game_data['Offense']=='Florida'].reset_index().drop(columns='index')
 
-# row = [5,'UTH',1,'O']
-
-# Experiment with excel files
-df = pd.DataFrame([['=SUM(1,1)','=SUM(A2+1)'],['=SUM(1,1)','=A3-1']], columns=['Foo','Bar'])
-with pd.ExcelWriter("experiment.xlsx") as writer:
-    df.to_excel(writer)
+# Not ready for prime time
+primary_array=np.array([
+    [5,'UTH',1,'O',primary_off.loc[0]['Down'],primary_off.loc[0]['Distance'],]
+    ])
+# for i in [1,primary_off.size[0]]:
+#     row = [5,'UTH',i,'O']
