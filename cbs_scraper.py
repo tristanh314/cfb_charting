1de0ccfdaa363e3e245b1eb0faa20addda5bab3f
tristanh314 <tristanh314@gmail.com>
2024-01-24 21:37:03 -0800

    Started function to scrape CBS play-by-play html

---
+++ cbs_scraper.py
@@ -0,0 +1,36 @@
+# Import Dependencies
+
+import pandas as pd
+import re
+from bs4 import BeautifulSoup
+
+def scrape_cbs(html_file):
+    """
+    Input: Relative file path for donwloaded html of college football plays from a game recorded at cbssports.com.
+    Output: Dataframes containing the data for the home team's offensive and defensive plays. 
+    """
+
+    # Store html from the downloaded page as a soup object.
+    page=open(html_file)
+    plays_soup = BeautifulSoup(page.read(), 'html.parser')
+
+    # Scrape the name and abbreviation for the teams
+    away_info = plays_soup.find('div', class_='hud-table-cell team-name-container full away')
+    away_abbr = away_info.find('div', class_='abbr').text
+    away_abbr = away_abbr.replace('\n', '')
+    away_abbr = away_abbr.replace(' ', '')
+    away_team = away_info.find('div', class_='city').text
+    away_team = away_team.replace('\n', '')
+    away_team = away_team.replace(' ', '')    
+    
+    home_info = plays_soup.find('div', class_='hud-table-cell team-name-container full home')
+    home_abbr = home_info.find('div', class_='abbr').text
+    home_abbr = home_abbr.replace('\n', '')
+    home_abbr = home_abbr.replace(' ', '')
+    home_team = home_info.find('div', class_='city').text
+    home_team = home_team.replace('\n', '')
+    home_team = home_team.replace(' ', '')
+
+    print(f"{away_abbr}, {away_team}, {home_abbr}, {home_team}")
+
+scrape_cbs('20230909_IDAHO@NEVADA.html')
