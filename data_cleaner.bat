37a6a66eda2e84a92c55bfe8fb60b4308e7efb09
tristanh314 <tristanh314@gmail.com>
2024-04-16 10:46:06 -0700

    Made changes for collaborators to more easily use for their own installation.

--- data_cleaner.bat
+++ data_cleaner.bat
@@ -1,2 +1,5 @@
 @echo off
-call "chart_cfb\Scripts\activate.bat" & python data_cleaner_openpyxl.py %*
+@REM This line for Tristan
+@REM call "chart_cfb\Scripts\activate.bat" & python data_cleaner_openpyxl.py %*
+@REM This line for hythloday
+python data_cleaner_openpyxl.py %*
